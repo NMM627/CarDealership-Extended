5064dc7df2da9cba345552ab05c7b769b67fef45
NMM627 <nmulhern2703@gmail.com>
2024-05-16 15:48:25 -0400

    finished phase 2

---
+++ src/main/java/com/pluralsight/ContractDataManager.java
@@ -0,0 +1,54 @@
+package com.pluralsight;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ContractDataManager {
+    public void saveContract(Contract parameter){
+        try {
+            FileWriter fw=new FileWriter("contract.csv",true);
+            BufferedWriter bw=new BufferedWriter(fw);
+
+            if (parameter instanceof LeaseContract) {
+                LeaseContract leaseContract = (LeaseContract) parameter;
+                bw.newLine();
+                bw.write(leaseContract.getCustomerName());
+                bw.write("|");
+                bw.write(leaseContract.getDate());
+                bw.write("|");
+                bw.write(leaseContract.getCustomerEmail());
+                bw.write("|");
+                bw.write(String.valueOf(leaseContract.getLeaseFee()));
+                bw.write("|");
+                bw.write(String.valueOf(leaseContract.getExpectedEndingValue()));
+                bw.write("|");
+                bw.write(leaseContract.getVehicleSold().toString());
+            }
+            if (parameter instanceof SalesContract){
+                SalesContract salesContract = (SalesContract) parameter;
+                bw.newLine();
+                bw.write(salesContract.getCustomerName());
+                bw.write("|");
+                bw.write(salesContract.getDate());
+                bw.write("|");
+                bw.write(String.valueOf(salesContract.getSalesTaxAmount()));
+                bw.write("|");
+                bw.write(String.valueOf(salesContract.getProcessingFee()));
+                bw.write("|");
+                bw.write(String.valueOf(salesContract.getRecordingFee()));
+                bw.write("|");
+                bw.write(String.valueOf(salesContract.isFinanced()));
+                bw.write("|");
+                bw.write(String.valueOf(salesContract.getVehicleSold()));
+            }
+
+
+            bw.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    }
+
